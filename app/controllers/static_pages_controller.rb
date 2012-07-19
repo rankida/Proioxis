b3dd78ba04043a68654e21dc5adf4f7d0f493027
David Rankin <Rankida@gmail.com>
2012-07-19 21:38:11 +0100

    Added static pages and bootstrap to the skeleton

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def not_implemented
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Proioxis::Application.routes.draw do
   resources :users
+  
+  root to: 'static_pages#home'
+
+  match '/signup',  to: 'static_pages#not_implemented'
+  match '/signin',  to: 'static_pages#not_implemented'
+  match '/signout', to: 'static_pages#not_implemented', via: :delete
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
