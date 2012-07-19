360af334ca9d1008685df5d6183694d0b4c74450
David Rankin <Rankida@gmail.com>
2012-07-19 20:30:39 +0100

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Proioxis::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proioxis::Application.config.secret_token = 'ef254e2eb57be5db5ef64a1f38ba57c6fe688ec130349dbdcfbd97a020ae585ee429bcd6fb75eb8f1d89180143d12ab9c92be87594a12dab7958417b5985ac22'
