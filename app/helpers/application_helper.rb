2e222efad22c7b5adde5605a6a53b8041956ced6
David Rankin <Rankida@gmail.com>
2012-07-21 21:26:05 +0100

    Half way through adding (broken) specs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	# Returns the full title on a per-page basis
 	def full_title(page_title)
-		base_title = "Rankida's Rails Tutorial Attempt"
+		base_title = "Proioxis"
 		if page_title.empty?
 			base_title
 		else
---
+++ spec/support/utilities.rb
@@ -0,0 +1,25 @@
+include ApplicationHelper
+
+def full_title(page_title)
+	base_title = "Proioxis"
+	if page_title.nil? || page_title.empty?
+		base_title
+	else
+		"#{base_title} | #{page_title}"
+	end
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+def sign_in(user)
+	visit signin_path
+	fill_in "Email",    with: user.email
+	fill_in "Password", with: user.password
+	click_button "Sign in"
+	# Sign in when not using Capybara as well
+	cookies[:remember_token] = user.remember_token
+end
