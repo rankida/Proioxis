14254978121f8eebc67057a89e32ab2386b15e25
David Rankin <Rankida@gmail.com>
2012-07-21 21:38:05 +0100

    Created Session model and matching views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Proioxis::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destory]
   
   root to: 'static_pages#home'
 
-  match '/signup',  to: 'static_pages#not_implemented'
-  match '/signin',  to: 'static_pages#not_implemented'
-  match '/signout', to: 'static_pages#not_implemented', via: :delete
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
