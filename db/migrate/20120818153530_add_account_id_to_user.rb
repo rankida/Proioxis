7a50a94cefa5c39993928122c0f69b60a7d98c95
David Rankin <Rankida@gmail.com>
2012-08-19 15:49:00 +0100

    Dirty little mod to migration to make sure the account_id of a user is not null

--- db/migrate/20120818153530_add_account_id_to_user.rb
+++ db/migrate/20120818153530_add_account_id_to_user.rb
@@ -1,7 +1,7 @@
 class AddAccountIdToUser < ActiveRecord::Migration
   def change
     change_table :users do |t|
-      t.integer :account_id
+      t.integer :account_id, :null => false
     end
   end
 end
